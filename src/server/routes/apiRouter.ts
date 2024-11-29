9bf28be4a6c741e9ef29b56b96ea7e1328210061
Colin Rooney <102275700+12mv2@users.noreply.github.com>
2024-11-28 23:01:43 -0700

    fix: Express middleware and routing issues
    - Add proper default export to testMiddleware

--- src/server/routes/apiRouter.ts
+++ src/server/routes/apiRouter.ts
@@ -1,14 +1,9 @@
 // Import and configuration 
 import express from 'express';
 import { Request, Response, NextFunction } from "express";
+import testMiddleware from '../controller/testMiddleware';
 
-const path = require('path');
 const router = express.Router();
-
-// Import controllers 
-const testMiddleware = require('../controller/testMiddleware.ts');
-
-// Routes 
 
 // [Note] This is a test route to check if the server works. 
 router.get('/test', testMiddleware.testFunction, (req: Request, res: Response, next: NextFunction) => {
