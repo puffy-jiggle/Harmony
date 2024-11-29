3b892b1654c8d437ba79d4cea76777bf92cd0a81
Colin Rooney <102275700+12mv2@users.noreply.github.com>
2024-11-28 19:19:48 -0700

    feature- Implement tailwind and daiseyUI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className="p-8">
       <h1>Welcome to HarmonyMaker</h1>
+      <p className="mt-4">
+        <Link to="/test" className="btn btn-primary">Go to Test Component</Link>
+      </p>
     </div>
   );
 };
 
 export default App;
+
+
+// import React from "react";
+// import { useEffect } from "react";
+// import TestComponent from "./TestComponent"; // Import your TestComponent
+
+// const App: React.FC = () => {
+//   useEffect(() => {
+//     console.log("App component mounted"); // Example useEffect for future functionality
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Welcome to HarmonyMaker</h1>
+//       <TestComponent /> {/* Render TestComponent here */}
+//     </div>
+//   );
+// };
+
+// export default App;
